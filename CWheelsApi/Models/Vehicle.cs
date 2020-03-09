ab0af9b56e5936786f5e2b0291e41e1a4125d3d1
Alberto González Balderas <albertoglezb@hotmail.com>
2020-03-09 19:08:55 +0100

    Se agregó la validación de datos requeridos de la clase vehículo. Se terminó la clase: 'Implement model validations in web api'.

--- CWheelsApi/Models/Vehicle.cs
+++ CWheelsApi/Models/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,13 @@ namespace CWheelsApi.Models
     {
         public int Id { get; set; }
         
+        [Required(ErrorMessage ="Title should not be null or empty")]
         public string Title { get; set; }
 
+        [Required]
         public double Price { get; set; }
 
+        [Required]
         public string Color { get; set; }
     }
 }
