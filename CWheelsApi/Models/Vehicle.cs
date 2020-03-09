9aa7ba1cd6ecf0d8c322a1a6246e643c852c594d
Alberto González Balderas <albertoglezb@hotmail.com>
2020-03-09 19:40:16 +0100

    Se agregaron los demás modelos para completar el proyecto. Se terminó la clase: 'Adding Models'.

--- CWheelsApi/Models/Vehicle.cs
+++ CWheelsApi/Models/Vehicle.cs
@@ -9,14 +9,39 @@ namespace CWheelsApi.Models
     public class Vehicle
     {
         public int Id { get; set; }
-        
-        [Required(ErrorMessage ="Title should not be null or empty")]
+
+        [Required(ErrorMessage = "Title should not be null or empty")]
         public string Title { get; set; }
+
+
+        public string Description { get; set; }
+
 
         [Required]
         public double Price { get; set; }
 
+        public string Model { get; set; }
+
+        public string Engine { get; set; }
+
         [Required]
         public string Color { get; set; }
+
+        public string Company { get; set; }
+
+        public DateTime DatePosted { get; set; }
+
+        public bool IsHotAndNew { get; set; }
+
+        public bool IsFeatured { get; set; }
+
+        public string Location { get; set; }
+
+
+        public int UserId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public ICollection<Image> Images { get; set; }
     }
 }
