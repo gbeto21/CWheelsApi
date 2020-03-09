947372a012ba28425966d39be33d382f14b91f5e
Alberto González Balderas <albertoglezb@hotmail.com>
2020-03-09 19:56:07 +0100

    Se agregó el controlador para las categorías. Se terminó la clase: 'Get categories'.

---
+++ CWheelsApi/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CWheelsApi.DataBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CWheelsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : Controller
+    {
+        private CWheelsDbContext _cwheelsDbContext;
+
+        public CategoriesController(CWheelsDbContext cWheelsDbContext)
+        {
+            _cwheelsDbContext = cWheelsDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var categories = _cwheelsDbContext.Categories;
+            return Ok(categories);
+        }
+    }
+}
