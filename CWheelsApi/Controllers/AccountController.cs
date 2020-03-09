dca076cb3501798a785ff30e0db1998369c951a9
Alberto González Balderas <albertoglezb@hotmail.com>
2020-03-09 22:36:02 +0100

    Se modificó el controlador para poder agregar usuarios a la base de datos. Se termino la clase: 'Register Method'.

---
+++ CWheelsApi/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CWheelsApi.DataBase;
+using CWheelsApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CWheelsApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        private CWheelsDbContext _cWheelsDbContext;
+
+        public AccountController(CWheelsDbContext cWheelsDbContext)
+        {
+            _cWheelsDbContext = cWheelsDbContext;
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromBody]User user)
+        {
+            var userWithSameEmail = _cWheelsDbContext.Users.Where(u => u.Email == user.Email).SingleOrDefault();
+            if (userWithSameEmail != null)
+                return BadRequest("User with same email already exist");
+
+            var userOb = new User()
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+
+            };
+
+            _cWheelsDbContext.Users.Add(userOb);
+            _cWheelsDbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+    }
+}
